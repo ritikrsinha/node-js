1ffbe6d963ba86395f84b8e388bed81c1dd8aecf
ritik sinha <ritikxsinha@gmail.com>
2022-10-13 16:05:03 +0530

    localhost:4000

---
+++ javascript/node.js
@@ -0,0 +1,49 @@
+// const fs = require('fs');
+
+// fs.writeFileSync('hello.txt', "Hello from node.js");
+
+
+console.log('a');
+
+console.log('b');
+
+setTimeOut(() => console.log('c'), 3000)
+
+setTimeOut(() => console.log('d'), 0)
+
+console.log('e');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- node.js
+++
@@ -1,5 +0,0 @@
-// const fs = require('fs');
-
-// fs.writeFileSync('hello.txt', "Hello from node.js");
-
-console.log('hello world')
